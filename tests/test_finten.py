5149ecb4078f36e5e00edb7ff6a1f55320cf60df
Àlex Encalado Masià <faraday@MacBookBPdeAlex.home>
2020-11-08 09:02:41 +0100

    feat: Added connection to FinTen API

--- tests/test_finten.py
+++ tests/test_finten.py
@@ -1,9 +1,15 @@
 import pytest
-from pifinance.finten import FinTen
+from pyfinance.finten import FinTen
 
 
-def test_is_backend_reachable():
-
+def test_is_reachable():
     finten = FinTen()
-    assert finten._is_backend_reachable()
+    assert finten._is_reachable()
+    finten.URI = "https://www.google.com"
+    assert finten._is_reachable() == False
+
+
+def test_login():
+    finten = FinTen()
+    finten.set_key(API_KEY="foo")
 
